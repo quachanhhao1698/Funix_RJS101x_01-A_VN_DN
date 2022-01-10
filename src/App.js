f5bb1e241b60f656c8550ddc2eb3cae65cfd97f7
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-01-11 06:06:43 +0700

    Fix css in App.css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
