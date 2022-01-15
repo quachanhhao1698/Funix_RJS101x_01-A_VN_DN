9d0546a8f38502fc2f7904de81426d7a46a51b5b
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-01-16 00:20:37 +0700

    Add option column in StaffListComponent.js file

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle,Button,ButtonGroup} from 'reactstrap';
+import {Card,CardImg,CardBody,CardTitle,Button,ButtonGroup,Col,Row,Container} from 'reactstrap';
 import StaffDetail from "./StaffDetailComponent";
 import $ from 'jquery';
 import '../App.css';
@@ -10,9 +10,14 @@ class StaffList extends Component{
 
     this.state = {
         selectedStaff:null,
-        column:3  
+        column:3,
+        selectedClolumn:{
+            desktop: 3,
+            tablet: 6,
+            mobile: 12
+        }
         };
-
+        this.onClickColBack = this.onClickColBack.bind(this);
         this.onClickCol2 = this.onClickCol2.bind(this);
         this.onClickCol3 = this.onClickCol3.bind(this);
         this.onClickCol4 = this.onClickCol4.bind(this);
@@ -23,70 +28,116 @@ class StaffList extends Component{
         this.setState({selectedStaff: staffs});
     }
 
+    
+
     onClickCol2() {
-        this.setState({column:6});
+        this.setState({
+            // column:6,
+            selectedClolumn:{
+                desktop: 6,
+                tablet: 6,
+                mobile: 6
+            }
+        });
     }
 
     onClickCol3() {
-        this.setState({column:4});
+        this.setState({
+            // column:4,
+            selectedClolumn:{
+                desktop: 4,
+                tablet: 4,
+                mobile: 4
+            }});
     }
 
     onClickCol4() {
-        this.setState({column:3});
+        this.setState({
+            // column:3,
+            selectedClolumn:{
+                desktop: 3,
+                tablet: 3,
+                mobile: 3
+            }});
     }
     
     onClickCol6() {
-        this.setState({column:2});
+        this.setState({
+            // column:2,
+            selectedClolumn:{
+                desktop: 2,
+                tablet: 2,
+                mobile: 2
+            }});
+    }
 
+    onClickColBack() {
+        this.setState({
+            // column:6,
+            selectedClolumn:{
+                desktop: 3,
+                tablet: 6,
+                mobile: 12
+            }
+        });
     }
 
     render() {
 
-        let classname = 'mt-4 col-12 col-md-6 col-lg-';
-        classname += this.state.column;
+        // let classname = 'col-lg-';
+        // classname += this.state.column;
 
         const staffList=this.props.staffs.map((staffs)=> {
             return (
-                <div  key={staffs.id} className={classname}>
+                <Col
+                    key={staffs.id}
+                    xs={this.state.selectedClolumn.mobile}
+                    md={this.state.selectedClolumn.tablet}
+                    lg={this.state.selectedClolumn.desktop}                
+                    className={"mt-3"}
+                    // className={classname}
+                    >
+
                     <Card onClick = {() => this.onStaffSelect(staffs)}>
                         <CardImg width="100%" src={staffs.image} alt={staffs.name} />
-                        <CardBody>
-                            <CardTitle >{staffs.name}</CardTitle>
+                        <CardBody width="100%">
+                            <CardTitle>{staffs.name}</CardTitle>
                         </CardBody>
                     </Card>
 
-                </div>
+                </Col>
             );
         });
 
 
 
         return(
-            <div className="container">
+            <Container >
 
-                    <div className="row">
+                    <Row className={"mt-3"}>
                         <p>Chọn số cột :</p>
                         <ButtonGroup className="col-2">
+                        <Button onClick={this.onClickColBack}>Default</Button>
                         <Button onClick={this.onClickCol2}>2</Button>
                         <Button onClick={this.onClickCol3}>3</Button>
-                        <Button onClick={this.onClickCol4}>4</Button>
+                        {/* <Button onClick={this.onClickCol4}>4</Button> */}
                         <Button onClick={this.onClickCol6}>6</Button>
                         </ButtonGroup>
 
-                    </div>
+                    </Row>
                     
-                    <div className="row">
+                    <Row>
                         {staffList}
-                    </div>
+                    </Row>
 
-                    <div>
+                    <Row>
                         <p>Bấm vào tên nhân viên để xem thông tin</p>
-                    </div>
-                    <div className="row">
+                    </Row>
+                    <Row>
                         <StaffDetail selectedStaff={this.state.selectedStaff}/>
-                    </div>
+                    </Row>
                 
-            </div>
+            </Container>
         );
     }
 }
