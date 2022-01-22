88637e16e7dda128918792395b834ffae27f0bc7
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-01-22 15:18:31 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle,Breadcrumb,BreadcrumbItem } from 'reactstrap';
 import dateFormat from 'dateformat';
-
-
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}) {
@@ -60,22 +59,31 @@ import dateFormat from 'dateformat';
             dish = (
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             )
           
         } else {
             dish = <div></div>
         }
+
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                </div>
+
                 {dish}
             </div>
         );
 
     }
-    
-    
-
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React,{Component}        from 'react';
 import {Switch,Route,Redirect}  from 'react-router-dom';
 import Home         from './HomeComponent';
 import Menu         from './MenuComponent';
+import DishDetail   from  './DishDetailComponent';
 import Contact      from './ContactComponent';
 import Header       from './HeaderComponent';
 import Footer       from './FooterComponent';
@@ -17,7 +18,7 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      commnets: COMMENTS,
+      comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS
     };
@@ -34,12 +35,23 @@ class Main extends Component {
         />
       )
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail
+            dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          />
+      );
+    };
+
     return (
       <div >
         <Header/>        
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
