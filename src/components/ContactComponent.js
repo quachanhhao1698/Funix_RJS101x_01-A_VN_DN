8037e28ae50d3abf9b6c982a60765a9cdca3020f
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-02-09 22:10:07 +0700

    Control form validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Breadcrumb , BreadcrumbItem,Button,Form ,FormGroup,Label,Input,Col } from 'reactstrap';
+import { Breadcrumb , BreadcrumbItem,Button,Form ,FormGroup,Label,Input,Col,Row,FormFeedback } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Field } from 'react-redux-form';
 
 class Contact extends Component{
 
@@ -14,10 +15,17 @@ class Contact extends Component{
                 email: '',
                 agree: false,
                 contactType: 'Tel.',
-                message: ''
+                message: '',
+                touched: {
+                    firstname: false,
+                    lastname: false,
+                    telnum: false,
+                    email: false
+                }
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
 
     handleInputChange(event) {
@@ -36,7 +44,52 @@ class Contact extends Component{
         event.preventDefault();
     }
 
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: {
+                ...this.state.touched,[field]: true
+            }
+        });
+    }
+
+    validate(firstname,lastname,telnum,email) {
+        const error = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: ''
+        };
+
+        if(this.state.touched.firstname && firstname.length < 3) {
+            error.firstname = "First Name should be >= 3 characters !"
+        }
+        else if(this.state.touched.firstname && firstname.length > 10) {
+            error.firstname = "First Name should be <= 10 characters !"
+        }
+
+        if(this.state.touched.lastname && lastname.length < 3) {
+            error.lastname = "Last Name should be >= 3 characters !"
+        }
+        else if(this.state.touched.lastname && lastname.length > 10) {
+            error.lastname = "Last Name should be <= 10 characters !"
+        }
+
+        const reg = /^\d+$/;
+        if(this.state.touched.telnum && !reg.test(telnum)) {
+            error.telnum = "Tel. Number  should contain only numbers !"
+        }
+
+        if(this.state.touched.email && email.split('').filter(x => x === '@').length != 1) {
+            error.email = "Email should contain a @ !"
+        }
+
+        return error;
+
+    }
+
     render(){
+
+        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email)
         return(
             <div className="container">
                 <div className="row">
@@ -92,7 +145,13 @@ class Contact extends Component{
                                             name='firstname'
                                             placeholder='First Name'
                                             value={this.state.firstname}
+                                            valid={errors.firstname === ''}
+                                            invalid={errors.firstname !==''}
+                                            onBlur={this.handleBlur('firstname')}
                                             onChange={this.handleInputChange} />
+                                    <FormFeedback>
+                                        {errors.firstname}
+                                    </FormFeedback>
                                 </Col>
                             </FormGroup>
 
@@ -104,7 +163,13 @@ class Contact extends Component{
                                             name='lastname'
                                             placeholder='Last Name'
                                             value={this.state.lastname}
+                                            valid={errors.lastname === ''}
+                                            invalid={errors.lastname !==''}
+                                            onBlur={this.handleBlur('lastname')}
                                             onChange={this.handleInputChange} />
+                                    <FormFeedback>
+                                        {errors.lastname}
+                                    </FormFeedback>
                                 </Col>
                             </FormGroup>   
 
@@ -116,7 +181,13 @@ class Contact extends Component{
                                             name='telnum'
                                             placeholder='Tel. Number'
                                             value={this.state.telnum}
+                                            valid={errors.telnum === ''}
+                                            invalid={errors.telnum !==''}
+                                            onBlur={this.handleBlur('telnum')}
                                             onChange={this.handleInputChange} />
+                                    <FormFeedback>
+                                        {errors.telnum}
+                                    </FormFeedback>
                                 </Col>
                             </FormGroup>
 
@@ -128,7 +199,13 @@ class Contact extends Component{
                                             name='email'
                                             placeholder='Email'
                                             value={this.state.email}
+                                            valid={errors.email === ''}
+                                            invalid={errors.email !==''}
+                                            onBlur={this.handleBlur('email')}
                                             onChange={this.handleInputChange} />
+                                    <FormFeedback>
+                                        {errors.email}
+                                    </FormFeedback>
                                 </Col>
                             </FormGroup>
 
